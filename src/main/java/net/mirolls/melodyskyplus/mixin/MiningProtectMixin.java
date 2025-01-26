b1edbfbb36cf5500d52a7aa1c014049fff0e828e
liangmiQwQ <mi_yyds@126.com>
2025-01-26 09:08:27 +0800

    feat: init

---
+++ src/main/java/net/mirolls/melodyskyplus/mixin/MiningProtectMixin.java
@@ -0,0 +1,26 @@
+package net.mirolls.melodyskyplus.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.Melody.Event.value.Option;
+import xyz.Melody.module.modules.macros.Mining.MiningProtect;
+
+//@Mixin(MiningProtect.class)
+public class MiningProtectMixin {
+//  public Option<Boolean> lookAt = new Option<>("Look at him", true);
+
+  //  @Inject(method = "checkPause",
+//      at = @At(value = "INVOKE", target = "Ljava/lang/StringBuilder;toString()Ljava/lang/String;")
+//      at = @At(value = "RETURN")
+//      ,locals = LocalCapture.CAPTURE_FAILSOFT
+//  )
+//  private void checkPause(CallbackInfo ci) {
+//
+//
+//  }
+//  @Inject(method = "onDisable", at = @At(value = "HEAD"))
+//  private void onDisable(CallbackInfo ci) {
+//  }
+}
