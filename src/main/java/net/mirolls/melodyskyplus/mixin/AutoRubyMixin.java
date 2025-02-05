6ae4be2f5487ad4e188cd4fea04dfe1938a8fe19
liangmiQwQ <mi_yyds@126.com>
2025-02-05 10:07:23 +0800

    feat: remove something

--- src/main/java/net/mirolls/melodyskyplus/mixin/AutoRubyMixin.java
+++ src/main/java/net/mirolls/melodyskyplus/mixin/AutoRubyMixin.java
@@ -1,12 +1,13 @@
 package net.mirolls.melodyskyplus.mixin;
 
-import net.minecraft.util.BlockPos;
 import net.mirolls.melodyskyplus.modules.Failsafe;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.Melody.Event.events.world.EventTick;
+import xyz.Melody.Utils.timer.TimerUtil;
 import xyz.Melody.module.modules.macros.Mining.AutoRuby;
 
 import java.util.Objects;
@@ -16,9 +17,19 @@ public class AutoRubyMixin {
   @Shadow
   private boolean etherWarped;
 
-  @Inject(method = "etherWarp", at = @At(value = "HEAD"), remap = false)
+  /* @Inject(method = "etherWarp", at = @At(value = "HEAD"), remap = false)
   private void etherWarp(BlockPos pos, CallbackInfo ci) {
     if (!this.etherWarped) {
+      Objects.requireNonNull(Failsafe.getINSTANCE()).lastLegitTeleport = Failsafe.getINSTANCE().nowTick;
+    }
+  } */
+
+  @Shadow
+  private TimerUtil timer;
+
+  @Inject(method = "idk", at = @At("HEAD"), remap = false)
+  private void idk(EventTick event, CallbackInfo ci) {
+    if (this.timer.hasReached(75)) {
       Objects.requireNonNull(Failsafe.getINSTANCE()).lastLegitTeleport = Failsafe.getINSTANCE().nowTick;
     }
   }
