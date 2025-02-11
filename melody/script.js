51f1d89d7d57061fad2ba0b66d01d14400b24329
liangmiQwQ <mi_yyds@126.com>
2025-02-11 17:06:26 +0800

    feat: remove auth for test

--- melody/script.js
+++ melody/script.js
@@ -1,16 +1,14 @@
 function isRemappingEnabledForClass(node) {
     var flag1 = node.name.startsWith("net/mirolls/melodyskyplus/client/AntiBug");
-    var flag2 = node.name.startsWith("net/mirolls/melodyskyplus/client/AntiRat");
     var flag3 = node.name.startsWith("net/mirolls/melodyskyplus/modules/MelodyPlusModules");
     var flag4 = node.name.startsWith("net/mirolls/melodyskyplus/Verify");
     var flag5 = node.name.startsWith("net/mirolls/melodyskyplus/client/Bug");
-    return flag1 || flag2 || flag3 || flag4 || flag5;
+    return flag1 || flag3 || flag4 || flag5;
 }
 function isObfuscatorEnabledForClass(node) {
     var flag1 = node.name.startsWith("net/mirolls/melodyskyplus/client/AntiBug");
-    var flag2 = node.name.startsWith("net/mirolls/melodyskyplus/client/AntiRat");
     var flag3 = node.name.startsWith("net/mirolls/melodyskyplus/modules/MelodyPlusModules");
     var flag4 = node.name.startsWith("net/mirolls/melodyskyplus/Verify");
     var flag5 = node.name.startsWith("net/mirolls/melodyskyplus/client/Bug");
-    return flag1 || flag2 || flag3 || flag4 || flag5;
+    return flag1 || flag3 || flag4 || flag5;
 }
