be1fcdcafa9c90ed97c369d5818551790d63dd99
liangmiQwQ <mi_yyds@126.com>
2025-02-12 21:12:06 +0800

    refactor: Free AntiRat.java!

--- melody/script2.js
+++
@@ -1,8 +0,0 @@
-function isRemappingEnabledForClass(node) {
-    var flag = node.name.startsWith("net/mirolls/melodyskyplus/client/AntiRat");
-    return flag
-}
-function isObfuscatorEnabledForClass(node) {
-    var flag = node.name.startsWith("net/mirolls/melodyskyplus/client/AntiRat");
-    return flag
-}
