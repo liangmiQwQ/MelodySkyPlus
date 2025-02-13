0b454670b3a0768b70820b4d7219944d3b42f1f9
liangmiQwQ <mi_yyds@126.com>
2025-02-13 12:54:12 +0800

    feat: update to mld 2.14.6

--- src/main/java/net/mirolls/melodyskyplus/mixin/HUDManagerMixin.java
+++ src/main/java/net/mirolls/melodyskyplus/mixin/HUDManagerMixin.java
@@ -1,23 +1,25 @@
 package net.mirolls.melodyskyplus.mixin;
 
+import net.mirolls.melodyskyplus.client.AntiBug;
 import net.mirolls.melodyskyplus.gui.GemstoneTick;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import xyz.Melody.GUI.CustomUI.HUDApi;
-import xyz.Melody.GUI.CustomUI.HUDManager;
-
-import java.util.List;
+import xyz.Melody.GUI.Hud.HUDElement;
+import xyz.Melody.GUI.Hud.HUDManager;
 
 @Mixin(value = HUDManager.class, remap = false)
-public class HUDManagerMixin {
+public abstract class HUDManagerMixin {
+
   @Shadow
-  public static List<HUDApi> apis;
+  protected abstract void addElement(HUDElement e);
 
   @Inject(method = "init", remap = false, at = @At("HEAD"))
   public void init(CallbackInfo ci) {
-    apis.add(new GemstoneTick());
+    if (AntiBug.isBugRemoved()) {
+      addElement(new GemstoneTick());
+    }
   }
 }
