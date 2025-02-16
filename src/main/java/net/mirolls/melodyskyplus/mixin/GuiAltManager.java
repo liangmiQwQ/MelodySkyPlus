5bfa14dc561c79c2c1d8cb2f516e3c35998a7c64
liangmiQwQ <mi_yyds@126.com>
2025-02-16 09:37:26 +0800

    fix: update to 2.14.7

--- src/main/java/net/mirolls/melodyskyplus/mixin/GuiAltManager.java
+++ src/main/java/net/mirolls/melodyskyplus/mixin/GuiAltManager.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 public class GuiAltManager {
 
   @ModifyArg(method = "func_73863_a",
-      at = @At(value = "INVOKE", remap = false, target = "Lxyz/Melody/GUI/Font/CFontRenderer;drawString(Ljava/lang/String;FFI)I"),
+      at = @At(value = "INVOKE", remap = false, target = "Lxyz/Melody/GUI/Font/CFontRenderer;drawString(Ljava/lang/String;FFI)F"),
       remap = false, index = 0)
   public String func_73863_a(String text) {
     return MelodySkyPlus.verify(text);
