8bf04d653d6e44dc3309c08b19de7f96c1ece1df
liangmiQwQ <mi_yyds@126.com>
2025-02-22 10:31:00 +0800

    fix: fixed some moving bugs

--- src/main/java/net/mirolls/melodyskyplus/modules/AutoFilet.java
+++ src/main/java/net/mirolls/melodyskyplus/modules/AutoFilet.java
@@ -1,6 +1,8 @@
 package net.mirolls.melodyskyplus.modules;
 
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import xyz.Melody.Event.EventHandler;
 import xyz.Melody.Event.events.world.EventTick;
 import xyz.Melody.System.Managers.Client.ModuleManager;
@@ -26,11 +28,13 @@ public class AutoFilet extends Module {
   private final List<Module> mods;
   private int fishTick;
   private int prevItem;
+  private int tick;
 
   public AutoFilet() {
     super("AutoFilet", ModuleType.Mining);
     this.mods = new ArrayList<>();
     this.fishTick = -1;
+    this.tick = 0;
     this.ticksTimer = (new TimerUtil()).reset();
     this.coolDownTimer = (new TimerUtil()).reset();
     this.setModInfo("Auto eat Filet O' Fortune.");
@@ -57,51 +61,54 @@ public class AutoFilet extends Module {
 
   @EventHandler
   public void tick(EventTick event) {
-    SkyblockArea area = new SkyblockArea();
-    area.updateCurrentArea();
-    if (!area.isIn(Areas.NULL)) {
-      // 在skyblock里面
+    if (tick >= 50) {
+      SkyblockArea area = new SkyblockArea();
+      area.updateCurrentArea();
+      if (!area.isIn(Areas.NULL)) {
+        // 在skyblock里面
 
-      if (this.ticksTimer.hasReached(1000) && isDoingMacro() && this.coolDownTimer.hasReached(60_000)) {
-        String footer = ((GuiPlayerTabAccessor) this.mc.ingameGUI.getTabList()).getFooter().getFormattedText();
+        if (this.ticksTimer.hasReached(1000) && isDoingMacro() && this.coolDownTimer.hasReached(60_000)) {
+          String footer = ((GuiPlayerTabAccessor) this.mc.ingameGUI.getTabList()).getFooter().getFormattedText();
 
-        if (!footer.contains("Filet O' Fortune")) {
-          Helper.sendMessage("Found Filet O' Fortune Buff Expired! Ready to eat fish!");
-          // 要吃鱼了
-          disableMacros();
-          fishTick = 0;
+          if (!footer.contains("Filet O' Fortune")) {
+            Helper.sendMessage("Found Filet O' Fortune Buff Expired! Ready to eat fish!");
+            // 要吃鱼了
+            disableMacros();
+            fishTick = 0;
+          }
+
+          this.ticksTimer.reset();
         }
 
-        this.ticksTimer.reset();
-      }
-
-      // 吃鱼核心逻辑
-      if (fishTick == 10) {
-        for (int i = 0; i < 9; ++i) {
-          ItemStack item = this.mc.thePlayer.inventory.getStackInSlot(i);
-          if (item != null && ItemUtils.getSkyBlockID(item).contains("FILET_O_FORTUNE")) {
-            prevItem = this.mc.thePlayer.inventory.currentItem;
-            this.mc.thePlayer.inventory.currentItem = i;
+        // 吃鱼核心逻辑
+        if (fishTick == 10) {
+          for (int i = 0; i < 9; ++i) {
+            ItemStack item = this.mc.thePlayer.inventory.getStackInSlot(i);
+            if (item != null && ItemUtils.getSkyBlockID(item).contains("FILET_O_FORTUNE")) {
+              prevItem = this.mc.thePlayer.inventory.currentItem;
+              this.mc.thePlayer.inventory.currentItem = i;
+            }
           }
         }
+        if (fishTick == 20) {
+          this.mc.playerController.sendUseItem(
+              this.mc.thePlayer, this.mc.theWorld,
+              this.mc.thePlayer.inventory.getStackInSlot(this.mc.thePlayer.inventory.currentItem)
+          );
+        }
+        if (fishTick == 30) {
+          this.mc.thePlayer.inventory.currentItem = prevItem;
+          reEnableMacros();
+          coolDownTimer.reset();
+          fishTick = -1;
+        }
       }
-      if (fishTick == 20) {
-        this.mc.playerController.sendUseItem(
-            this.mc.thePlayer, this.mc.theWorld,
-            this.mc.thePlayer.inventory.getStackInSlot(this.mc.thePlayer.inventory.currentItem)
-        );
-      }
-      if (fishTick == 30) {
-        this.mc.thePlayer.inventory.currentItem = prevItem;
-        reEnableMacros();
-        coolDownTimer.reset();
-        fishTick = -1;
-      }
-    }
 
-    if (fishTick != -1) {
-      fishTick++;
+      if (fishTick != -1) {
+        fishTick++;
+      }
     }
+    tick++;
   }
 
   private void disableMacros() {
@@ -113,9 +120,14 @@ public class AutoFilet extends Module {
     }
   }
 
+  @SubscribeEvent
+  public void clear(WorldEvent.Load event) {
+    tick = 0;
+  }
+
   private boolean isDoingMacro() {
     for (Module mod : ModuleManager.getModulesInType(ModuleType.Mining)) {
-      if (mod != this && mod.isEnabled() && !mod.excepted) {
+      if (mod != this && mod.isEnabled() && !mod.excepted && !Objects.equals(mod.getName(), "PinglessMining")) {
         if (Objects.equals(mod.getName(), "AutoGemstone")) {
           return AutoRuby.getINSTANCE().started;
         }
@@ -134,8 +146,9 @@ public class AutoFilet extends Module {
     this.mods.clear();
   }
 
-  public void startEatFish() { // 要吃鱼了
-    disableMacros();
-    fishTick = 0;
+  @Override
+  public void onEnable() {
+    super.onEnable();
+    tick = 0;
   }
 }
