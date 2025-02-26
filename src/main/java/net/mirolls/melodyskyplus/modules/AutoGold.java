7b8292118b58f6706fc6581dcc29c8dac8b3ff23
liangmiQwQ <mi_yyds@126.com>
2025-02-26 21:21:10 +0800

    refactor: refactor some pathfinder of AutoGold

--- src/main/java/net/mirolls/melodyskyplus/modules/AutoGold.java
+++ src/main/java/net/mirolls/melodyskyplus/modules/AutoGold.java
@@ -5,6 +5,7 @@ import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.MovingObjectPosition;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.mirolls.melodyskyplus.MelodySkyPlus;
@@ -17,6 +18,7 @@ import xyz.Melody.System.Managers.Client.ModuleManager;
 import xyz.Melody.System.Managers.Skyblock.Area.Areas;
 import xyz.Melody.System.Managers.Skyblock.Area.SkyblockArea;
 import xyz.Melody.Utils.Helper;
+import xyz.Melody.Utils.Vec3d;
 import xyz.Melody.Utils.game.item.ItemUtils;
 import xyz.Melody.Utils.math.Rotation;
 import xyz.Melody.Utils.math.RotationUtil;
@@ -71,7 +73,9 @@ public class AutoGold extends Module {
   }
 
   public void findGold() {
-    rotateDoneTick = 0;
+//    rotateDoneTick = 0;
+    rotateDoneTick = -2147483647;
+
     if (findingGoldTick < 0) {
       // 最复杂的部分
       findingGoldTick = 0;
@@ -258,7 +262,7 @@ public class AutoGold extends Module {
                 mc.thePlayer.getPosition().up().add(-1 * range.getValue(), -1 * range.getValue(), -1 * range.getValue()),
                 mc.thePlayer.getPosition().up().add(range.getValue(), range.getValue(), range.getValue())
             )) {
-              if (mc.theWorld.getBlockState(blockPos).getBlock() == Blocks.gold_block && RotationUtil.rayTrace(blockPos)) {
+              if (mc.theWorld.getBlockState(blockPos).getBlock() == Blocks.gold_block && rayTrace(blockPos)) {
                 this.mc.thePlayer.inventory.currentItem = prevItem;
                 resume();
                 return;
@@ -336,7 +340,7 @@ public class AutoGold extends Module {
             chekcedBlockPosList.add(blockPos);
 
             if (mc.theWorld.getBlockState(blockPos).getBlock() != Blocks.air) {
-              if (RotationUtil.rayTrace(blockPos)) {
+              if (rayTrace(blockPos)) {
                 if (mc.theWorld.getBlockState(blockPos.up()).getBlock() == Blocks.air && mc.theWorld.getBlockState(blockPos.up().up()).getBlock() == Blocks.air) {
                   int aroundBlocks = 0;
                   for (int i = 0; i < 4; i++) {
@@ -446,6 +450,46 @@ public class AutoGold extends Module {
     }
     walkTimer.reset();
   }
+
+  private boolean rayTrace(BlockPos blockPos) {
+    Vec3d target = null;
+    Vec3d[] var2 = Vec3d.points(blockPos);
+    int var3 = var2.length;
+    int var4 = 0;
+
+    while (var4 < var3) {
+      Vec3d vec = var2[var4];
+      MovingObjectPosition trajectory = RotationUtil.rayTrace(vec);
+      if (trajectory == null) {
+        target = vec;
+        break;
+      }
+
+      label69:
+      {
+        if (trajectory.entityHit == null || trajectory.entityHit == mc.thePlayer) {
+          if (trajectory.getBlockPos() == null) {
+            break label69;
+          }
+
+          boolean sameX = trajectory.getBlockPos().getX() == blockPos.getX();
+          boolean sameY = trajectory.getBlockPos().getY() == blockPos.getY();
+          boolean sameZ = trajectory.getBlockPos().getZ() == blockPos.getZ();
+          if (sameX && sameY && sameZ) {
+            break label69;
+          }
+        }
+
+        ++var4;
+        continue;
+      }
+
+      target = vec;
+      break;
+    }
+
+    return target != null;
+  }
 }
 
 class SidesAroundPlayer {
@@ -496,4 +540,6 @@ class SidesAroundPlayer {
 
     return yaw;
   }
+
+
 }
