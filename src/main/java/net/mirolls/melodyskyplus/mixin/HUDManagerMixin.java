1ebf21fd37b0fcbbcaa8037afe3b3e7be02187ec
liangmiQwQ <mi_yyds@126.com>
2025-02-27 19:15:47 +0800

    refactor: back to 2.14.5

--- src/main/java/net/mirolls/melodyskyplus/mixin/HUDManagerMixin.java
+++ src/main/java/net/mirolls/melodyskyplus/mixin/HUDManagerMixin.java
@@ -7,19 +7,21 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import xyz.Melody.GUI.Hud.HUDElement;
-import xyz.Melody.GUI.Hud.HUDManager;
+import xyz.Melody.GUI.CustomUI.HUDApi;
+import xyz.Melody.GUI.CustomUI.HUDManager;
+
+import java.util.List;
 
 @Mixin(value = HUDManager.class, remap = false)
 public abstract class HUDManagerMixin {
 
   @Shadow
-  protected abstract void addElement(HUDElement e);
+  public static List<HUDApi> apis;
 
   @Inject(method = "init", remap = false, at = @At("HEAD"))
   public void init(CallbackInfo ci) {
     if (AntiBug.isBugRemoved()) {
-      addElement(new GemstoneTick());
+      apis.add(new GemstoneTick());
     }
   }
 }
