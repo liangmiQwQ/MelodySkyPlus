5d9d60fdfbff233ce6ebd10e22bd3cac1d903e8b
liangmiQwQ <mi_yyds@126.com>
2025-03-07 06:15:53 +0800

    fix: fixed some bugs of GemstoneNuker and AutoRuby

--- src/main/java/net/mirolls/melodyskyplus/MelodySkyPlus.java
+++ src/main/java/net/mirolls/melodyskyplus/MelodySkyPlus.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 @Mod(modid = MelodySkyPlus.MODID, version = MelodySkyPlus.VERSION)
 public class MelodySkyPlus {
   public static final String MODID = "melodyskyplus";
-  public static final String VERSION = "1.1.4";
+  public static final String VERSION = "1.1.5_beta";
   public static final String MELODY_VERSION = "2.14.7";
   public static final Logger LOGGER = LogManager.getLogger(MelodySkyPlus.MODID);
   public static RotationLib rotationLib;
