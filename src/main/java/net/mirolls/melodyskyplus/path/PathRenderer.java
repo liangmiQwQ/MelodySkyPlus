133bdafd67ee3aba98c7fea9f3092820569a126c
liangmiQwQ <mi_yyds@126.com>
2025-03-08 12:38:24 +0800

    fix: fixed some bugs of finding path

--- src/main/java/net/mirolls/melodyskyplus/path/PathRenderer.java
+++ src/main/java/net/mirolls/melodyskyplus/path/PathRenderer.java
@@ -2,11 +2,12 @@ package net.mirolls.melodyskyplus.path;
 
 import net.minecraft.util.BlockPos;
 import net.minecraftforge.common.MinecraftForge;
+import xyz.Melody.Event.EventBus;
 import xyz.Melody.Event.EventHandler;
 import xyz.Melody.Event.events.rendering.EventRender3D;
-import xyz.Melody.Utils.Colors;
 import xyz.Melody.Utils.render.RenderUtil;
 
+import java.awt.*;
 import java.util.List;
 
 public class PathRenderer {
@@ -14,15 +15,16 @@ public class PathRenderer {
   private List<BlockPos> path = null;
 
   public PathRenderer() {
+    EventBus.getInstance().register(this);
     MinecraftForge.EVENT_BUS.register(this);
   }
 
   @EventHandler
   public void onRender(EventRender3D event) {
     if (target != null && path != null && !path.isEmpty()) {
-      RenderUtil.drawSolidBlockESP(target, Colors.GREEN.c, event.getPartialTicks());
+      RenderUtil.drawFullBlockESP(target, new Color(44, 105, 133, 100), event.getPartialTicks());
       for (BlockPos pos : path) {
-        RenderUtil.drawSolidBlockESP(pos, Colors.DARKBLUE.c, event.getPartialTicks());
+        RenderUtil.drawFullBlockESP(pos, new Color(8, 125, 13, 100), event.getPartialTicks());
       }
     }
   }
