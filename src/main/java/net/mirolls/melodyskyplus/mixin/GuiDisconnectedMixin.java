b94a5ad458d30e0db28b4da668ac8c26de3bebcd
liangmiQwQ <mi_yyds@126.com>
2025-03-11 20:05:56 +0800

    chore: update version to 1.1.6 feat: add AutoReconnect

---
+++ src/main/java/net/mirolls/melodyskyplus/mixin/GuiDisconnectedMixin.java
@@ -0,0 +1,8 @@
+package net.mirolls.melodyskyplus.mixin;
+
+import net.minecraft.client.gui.GuiDisconnected;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(GuiDisconnected.class)
+public class GuiDisconnectedMixin {
+}
