e13059e0506b17a895022ccdbc88199d3cd816c3
liangmiQwQ <mi_yyds@126.com>
2025-03-22 10:47:36 +0800

    feat: add basic autoHeat

--- src/main/java/net/mirolls/melodyskyplus/mixin/AutoRubyMixin.java
+++ src/main/java/net/mirolls/melodyskyplus/mixin/AutoRubyMixin.java
@@ -1,9 +1,14 @@
 package net.mirolls.melodyskyplus.mixin;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.inventory.GuiChest;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.ContainerChest;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
-import net.mirolls.melodyskyplus.MelodySkyPlus;
+import net.minecraft.util.StringUtils;
 import net.mirolls.melodyskyplus.libs.AutoRubyTimer;
 import net.mirolls.melodyskyplus.modules.Failsafe;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,7 +23,7 @@ import xyz.Melody.Event.value.Option;
 import xyz.Melody.Event.value.Value;
 import xyz.Melody.Utils.Helper;
 import xyz.Melody.Utils.game.ScoreboardUtils;
-import xyz.Melody.Utils.math.Rotation;
+import xyz.Melody.Utils.game.item.ItemUtils;
 import xyz.Melody.Utils.timer.TimerUtil;
 import xyz.Melody.module.modules.macros.Mining.AutoRuby;
 import xyz.Melody.module.modules.macros.Mining.GemstoneNuker;
@@ -32,10 +37,8 @@ import java.util.regex.Pattern;
 @SuppressWarnings("rawtypes")
 @Mixin(value = AutoRuby.class, remap = false)
 public class AutoRubyMixin {
-  private final Option<Boolean> melodySkyPlus$mineTop = new Option<>("MineTop", true);
 
-  private final Numbers<Double> melodySkyPlus$maxHeat = new Numbers<>("MaxHeat", 95.0, 1.0, 100.0, 1.0);
-  private final Numbers<Double> melodySkyPlus$minHeat = new Numbers<>("MinHeat", 90.0, 0.0, 99.0, 1.0);
+  private final Numbers<Double> melodySkyPlus$heatLimit = new Numbers<>("HeatLimit", 95.0, 1.0, 100.0, 1.0);
   @Shadow
   private TimerUtil ewTimer;
   @Shadow
@@ -44,41 +47,24 @@ public class AutoRubyMixin {
   private BlockPos nextBP;
   @Shadow
   private TimerUtil timer;
-  private boolean melodySkyPlus$jumping = false;
-  private TimerUtil melodySkyPlus$jumpTimer;
-
   private Option<Boolean> melodySkyPlus$autoHeat = null;
+  private int reactingTick = -1;
+  private int prevItem;
 
   @ModifyArg(method = "<init>", remap = false, at = @At(value = "INVOKE", target = "Lxyz/Melody/module/modules/macros/Mining/AutoRuby;addValues([Lxyz/Melody/Event/value/Value;)V"))
   private Value[] init(Value[] originalValues) {
-    melodySkyPlus$jumpTimer = new TimerUtil().reset();
     melodySkyPlus$autoHeat = new Option<>("AutoHeat", false, val -> {
       if (AutoRuby.getINSTANCE() != null) {
-        melodySkyPlus$maxHeat.setEnabled(val);
-        melodySkyPlus$minHeat.setEnabled(val);
-        melodySkyPlus$mineTop.setValue(val);
+        melodySkyPlus$heatLimit.setEnabled(val);
       }
     });
 
-    Value[] returnValues = Arrays.copyOf(originalValues, originalValues.length + 4);
+    Value[] returnValues = Arrays.copyOf(originalValues, originalValues.length + 2);
 
-    returnValues[returnValues.length - 4] = melodySkyPlus$autoHeat;
-    returnValues[returnValues.length - 3] = melodySkyPlus$minHeat;
-    returnValues[returnValues.length - 2] = melodySkyPlus$maxHeat;
-    returnValues[returnValues.length - 1] = melodySkyPlus$mineTop;
+    returnValues[returnValues.length - 2] = melodySkyPlus$autoHeat;
+    returnValues[returnValues.length - 1] = melodySkyPlus$heatLimit;
 
     return returnValues;
-  }
-
-  @Inject(method = "onEnable", at = @At("HEAD"))
-  private void onEnable(CallbackInfo ci) {
-    melodySkyPlus$jumping = false;
-  }
-
-  @Inject(method = "onDisable", at = @At("HEAD"))
-  private void onDisable(CallbackInfo ci) {
-    KeyBinding.setKeyBindState(Minecraft.getMinecraft().gameSettings.keyBindAttack.getKeyCode(), false);
-    melodySkyPlus$jumping = false;
   }
 
 
@@ -93,66 +79,90 @@ public class AutoRubyMixin {
       if (melodySkyPlus$autoHeat.getValue() && AutoRubyTimer.timer.hasReached(500)) {
         // 主要部分 处理AutoHeat
         List<String> scoreBoard = ScoreboardUtils.getScoreboard();
+
+        int heat = 0;
         for (String line : scoreBoard) {
           if (line.toLowerCase().contains("heat:")) {
-            int heat = melodySkyPlus$getHeat(line.replaceAll(".*Heat: §[a-f0-9]", ""));
-            if (AutoRuby.getINSTANCE().started) {
-              if (heat >= melodySkyPlus$maxHeat.getValue() && !melodySkyPlus$jumping && melodySkyPlus$jumpTimer.hasReached(20_000)) {
-                if (mc.thePlayer.posY <= 64 && mc.thePlayer.posY >= 64 - 6) {
-                  Helper.sendMessage("Found heat too high (" + heat + "), start to jump to make heat lower.");
-                  if (AutoRuby.getINSTANCE().started) {
-                    AutoRuby.getINSTANCE().started = false;
-                  }
-                  melodySkyPlus$jumping = true;
-                  melodySkyPlus$jumpTimer.reset();
-                  if (melodySkyPlus$mineTop.getValue()) {
-                    MelodySkyPlus.rotationLib.setCallBack(() -> {
-                      KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), true);
-                    });
-                    MelodySkyPlus.rotationLib.setTargetRotation(new Rotation(mc.thePlayer.rotationYaw, -90F));
-                    MelodySkyPlus.rotationLib.setSpeedCoefficient(2F);
-                    MelodySkyPlus.rotationLib.startRotating();
-                  }
-                }
-              }
-            } else {
-              if (melodySkyPlus$jumping) {
-                if (melodySkyPlus$minHeat.getValue() >= heat) {
-                  Helper.sendMessage("Found heat comfortable now (" + heat + "), macro resume.");
-                } else if (melodySkyPlus$jumpTimer.hasReached(10_000)) {
-                  Helper.sendMessage("Found jump too much, retry in 20s.");
-                } else {
-                  break;
-                }
-                melodySkyPlus$jumping = false;
-                KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), false);
-                new Thread(() -> {
-                  try {
-                    Thread.sleep(1000);
-                  } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                  }
-                  if (!AutoRuby.getINSTANCE().started) {
-                    AutoRuby.getINSTANCE().started = true;
-                  }
-                }).start();
+            heat = melodySkyPlus$getHeat(line.replaceAll(".*Heat: §[a-f0-9]", ""));
+            break;
+          }
+        }
+
+        if (AutoRuby.getINSTANCE().started) {
+          if (heat >= melodySkyPlus$heatLimit.getValue() && reactingTick == -1) {
+            if (mc.thePlayer.posY <= 64 && mc.thePlayer.posY >= 64 - 6) {
+              Helper.sendMessage("Found heat too high (" + heat + "), start to junk some water.");
+              if (AutoRuby.getINSTANCE().started) {
+                reactingTick = 0;
+                AutoRuby.getINSTANCE().started = false;
               }
             }
-
-            break;
           }
         }
       }
     }
 
-    if (mc.thePlayer.onGround && melodySkyPlus$jumping) {
-      if (melodySkyPlus$mineTop.getValue()) {
-        if (Math.abs(mc.thePlayer.rotationPitch - (-90F)) < 0.05) {
-          mc.thePlayer.jump();
+    if (reactingTick > -1) {
+      if (reactingTick == 10) {
+        for (int i = 0; i < 9; ++i) {
+          ItemStack item = mc.thePlayer.inventory.getStackInSlot(i);
+          if (item != null && ItemUtils.getSkyBlockID(item).startsWith("ABIPHONE")) {
+            prevItem = mc.thePlayer.inventory.currentItem;
+            mc.thePlayer.inventory.currentItem = i;
+          }
         }
-      } else {
-        mc.thePlayer.jump();
+      } else if (reactingTick == 20) {
+        ItemStack item = mc.thePlayer.inventory.getStackInSlot(mc.thePlayer.inventory.currentItem);
+        if (ItemUtils.getSkyBlockID(item).startsWith("ABIPHONE")) {
+          mc.playerController.sendUseItem(mc.thePlayer, mc.theWorld, mc.thePlayer.inventory.getStackInSlot(mc.thePlayer.inventory.currentItem));
+        } else {
+          Helper.sendMessage("Missing AbiPhone in hotbar.");
+          reactingTick = -1;
+          AutoRuby.getINSTANCE().started = true;
+        }
+      } else if (reactingTick == 30) {
+        GuiScreen gui = mc.currentScreen;
+        if (gui instanceof GuiChest) {
+          Container container = ((GuiChest) gui).inventorySlots;
+          if (container instanceof ContainerChest) {
+            String chestName = this.melodySkyPlus$getGuiName(gui);
+            if (chestName.startsWith("Abiphone")) {
+              for (Slot slot : container.inventorySlots) {
+                ItemStack item = slot.getStack(); // 获取item
+                if (StringUtils.stripControlCodes(item.getDisplayName()).equals("Alchemist")) {
+                  // 找到对应的人了
+                  melodySkyPlus$clickSlot(slot.slotNumber, 0, 0);
+                  break;
+                }
+              }
+            }
+          }
+        } else {
+          reactingTick = 19; // 重新返回上一步
+        }
+      } else if (reactingTick == 140) {
+        GuiScreen gui = mc.currentScreen;
+        if (gui instanceof GuiChest) {
+          Container container = ((GuiChest) gui).inventorySlots;
+          if (container instanceof ContainerChest) {
+            String chestName = this.melodySkyPlus$getGuiName(gui);
+            if (chestName.startsWith("Alchemist")) {
+              for (Slot slot : container.inventorySlots) {
+                // 买水
+                ItemStack item = slot.getStack(); // 获取item
+                if (StringUtils.stripControlCodes(item.getDisplayName()).equals("Water Bottle")) {
+                  // 找到了水
+                  break;
+                }
+              }
+            }
+          }
+        } else {
+          mc.thePlayer.closeScreen();
+          reactingTick = 11; // 重新返回第一步
+        }
       }
+      reactingTick++;
     }
   }
 
@@ -165,5 +175,14 @@ public class AutoRubyMixin {
     } else {
       throw new IllegalArgumentException("Error: No target heat found");
     }
+  }
+
+  public String melodySkyPlus$getGuiName(GuiScreen gui) {
+    return gui instanceof GuiChest ? ((ContainerChest) ((GuiChest) gui).inventorySlots).getLowerChestInventory().getDisplayName().getUnformattedText() : "";
+  }
+
+  private void melodySkyPlus$clickSlot(int slot, int button, int mode) {
+    Minecraft mc = Minecraft.getMinecraft();
+    mc.playerController.windowClick(mc.thePlayer.openContainer.windowId, slot, button, mode, mc.thePlayer);
   }
 }
