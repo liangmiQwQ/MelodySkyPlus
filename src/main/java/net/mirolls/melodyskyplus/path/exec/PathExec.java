142758ca6b1d7a4a2b95218d9aaafee2a7e67e88
liangmiQwQ <mi_yyds@126.com>
2025-04-03 06:15:30 +0800

    feat: add basic Exec

---
+++ src/main/java/net/mirolls/melodyskyplus/path/exec/PathExec.java
@@ -0,0 +1,54 @@
+package net.mirolls.melodyskyplus.path.exec;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.common.MinecraftForge;
+import net.mirolls.melodyskyplus.MelodySkyPlus;
+import net.mirolls.melodyskyplus.path.type.Node;
+import xyz.Melody.Event.EventBus;
+import xyz.Melody.Event.EventHandler;
+import xyz.Melody.Event.events.world.EventTick;
+import xyz.Melody.Utils.Vec3d;
+import xyz.Melody.Utils.math.RotationUtil;
+
+import java.util.List;
+
+public class PathExec {
+
+  public PathExec() {
+    EventBus.getInstance().register(this);
+    MinecraftForge.EVENT_BUS.register(this);
+  }
+
+  @EventHandler
+  public void onTick(EventTick event) {
+    Minecraft mc = Minecraft.getMinecraft();
+
+    List<Node> path = MelodySkyPlus.smartyPathFinder.path;
+
+    if (path != null && !path.isEmpty()) {
+      if (path.size() == 1) {
+        MelodySkyPlus.smartyPathFinder.clear();
+        return;
+      }
+
+      // 执行器核心 运行path
+      Node node = path.get(0);
+      Node nextNode = path.get(1);
+
+      // 移动玩家视角
+      mc.thePlayer.rotationPitch = RotationUtil.smoothRotation(mc.thePlayer.rotationPitch, node.getNextRotation().getPitch(), 1);
+      mc.thePlayer.rotationYaw = RotationUtil.smoothRotation(mc.thePlayer.rotationYaw, node.getNextRotation().getYaw(), 120);
+
+      // 控制行走
+      KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), true);
+
+      // 如果玩家走到了点则停止
+      Vec3d nextVec = Vec3d.ofCenter(nextNode.getPos());
+      if (Math.hypot(mc.thePlayer.posX - nextVec.getX(), mc.thePlayer.posZ - nextVec.getZ()) < 1.5) {
+        path.remove(0);
+      }
+    }
+  }
+
+}
