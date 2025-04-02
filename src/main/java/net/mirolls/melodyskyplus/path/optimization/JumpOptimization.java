297cfd817a1bc6c4899ccc5c0efbf649bd61a81b
liangmiQwQ <mi_yyds@126.com>
2025-04-03 06:15:30 +0800

    fix: fixed the bugs of using wrong Rotation

--- src/main/java/net/mirolls/melodyskyplus/path/optimization/JumpOptimization.java
+++ src/main/java/net/mirolls/melodyskyplus/path/optimization/JumpOptimization.java
@@ -1,6 +1,32 @@
 package net.mirolls.melodyskyplus.path.optimization;
 
+import net.mirolls.melodyskyplus.path.type.Jump;
+import net.mirolls.melodyskyplus.path.type.Node;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class JumpOptimization {
   // 优化跳跃
   // 原理: 检查每一个跳跃节点
+
+  public static final HashMap<Integer, Integer> JUMP_DISTANCE = new HashMap<>();
+
+  static {
+    JUMP_DISTANCE.put(1, 1);
+  }
+
+  public List<Node> optimize(List<Node> nodes) {
+    for (int i = 0; i < nodes.size(); i++) {
+      Node node = nodes.get(i);
+      if (node instanceof Jump && i > 1) {// 要至少前面有2个点
+        // 是跳跃类型节点
+        Node startNode = nodes.get(i - 1);
+        Node prevNode = nodes.get(i - 1);
+      }
+    }
+
+
+    return nodes;
+  }
 }
