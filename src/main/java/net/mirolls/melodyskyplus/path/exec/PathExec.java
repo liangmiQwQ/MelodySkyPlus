e05de1ca57228e26d1a9ca71207039f34c8a080c
liangmiQwQ <mi_yyds@126.com>
2025-04-04 21:50:26 +0800

    feat: add jumpExec

--- src/main/java/net/mirolls/melodyskyplus/path/exec/PathExec.java
+++ src/main/java/net/mirolls/melodyskyplus/path/exec/PathExec.java
@@ -7,6 +7,7 @@ import net.minecraft.util.BlockPos;
 import net.minecraft.util.MathHelper;
 import net.minecraftforge.common.MinecraftForge;
 import net.mirolls.melodyskyplus.MelodySkyPlus;
+import net.mirolls.melodyskyplus.path.type.Jump;
 import net.mirolls.melodyskyplus.path.type.Mine;
 import net.mirolls.melodyskyplus.path.type.Node;
 import net.mirolls.melodyskyplus.path.type.Walk;
@@ -46,6 +47,40 @@ public class PathExec {
       Node nextNode = path.get(1);
 
       if (nextNode instanceof Walk) {
+        walkExec(nextNode, mc, node);
+
+        // 这里给停止部分单独拉出来 是为了让Jump的代码复用
+        Vec3d nextVec = Vec3d.ofCenter(nextNode.getPos());
+        if (Math.hypot(mc.thePlayer.posX - nextVec.getX(), mc.thePlayer.posZ - nextVec.getZ()) < 1.5) {
+          path.remove(0);
+        }
+      } else if (nextNode instanceof Mine) {
+        mineExec(nextNode, mc, path);
+      } else if (nextNode instanceof Jump) {
+        jumpExec(nextNode, path, mc, node);
+      }
+    }
+  }
+
+  private void jumpExec(Node nextNode, List<Node> path, Minecraft mc, Node node) {
+    // 提前先转化成Jump类型 为了事后做用方便
+    Jump jumpNode = (Jump) nextNode;
+    Node endNode = path.get(2);
+
+
+    if (mc.thePlayer.onGround) {
+      Vec3d jumpVec = Vec3d.ofCenter(jumpNode.getPos());
+      if (Math.hypot(mc.thePlayer.posX - jumpVec.getX(), mc.thePlayer.posZ - jumpVec.getZ()) < jumpNode.jumpDistance) {
+        // 到达跳跃范围
+        mc.thePlayer.jump();
+      } else {
+        walkExec(jumpNode, mc, node);
+      }
+    } else {
+      // 如果玩家没有到位的
+      int x = (int) (mc.thePlayer.posX - mc.thePlayer.posX % 1 - 1);
+      int z = (int) (mc.thePlayer.posZ - mc.thePlayer.posZ % 1 - 1);
+      if (x != endNode.getPos().getX() || z != endNode.getPos().getZ()) {
         // 转换到角度
         Rotation rotation = RotationUtil.vec3ToRotation(Vec3d.ofCenter(nextNode.pos));
 
@@ -53,101 +88,117 @@ public class PathExec {
         mc.thePlayer.rotationPitch = smoothRotation(mc.thePlayer.rotationPitch, rotation.getPitch(), new Random().nextFloat() / 5);
         mc.thePlayer.rotationYaw = smoothRotation(mc.thePlayer.rotationYaw, rotation.getYaw(), 75F);
 
-        // 控制行走
         KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), true);
-        KeyBinding.setKeyBindState(mc.gameSettings.keyBindSneak.getKeyCode(), false);
-
-        // 利用node中记录的rotation与实际rotation的差值 通过a和d修正玩家位置
-        if (Math.abs(mc.thePlayer.rotationYaw - rotation.getYaw()) < 0.5) {
-          // 开始纠正位置
-          float yawShould = node.nextRotation.getYaw();
-          float yawNow = rotation.getYaw();
-
-          // 经过研究 发现如果正负号不统一的 并且角度小于-90度的 则需要将负号改为正号
-          if (yawNow < -90 && yawShould > 90) {
-            yawNow = -1 * yawNow;
-          } else if (yawNow > 90 && yawShould < -90) {
-            yawShould = -1 * yawShould;
-          }
-
-          // 如果 now - should是正的 则偏左 则需要往右移动
-          if ((yawNow - yawShould) > 2) {
-            MelodySkyPlus.LOGGER.info("Found Player offset to the left, start to go to the right.");
-            KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.getKeyCode(), true);
-          } else {
-            KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.getKeyCode(), false);
-          }
-
-          // 反之亦然
-          if ((yawNow - yawShould) < -2) {
-            MelodySkyPlus.LOGGER.info("Found Player offset to the right, start to go to the left.");
-            KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.getKeyCode(), true);
-          } else {
-            KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.getKeyCode(), false);
-          }
-        } else {
-          KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.getKeyCode(), false);
-          KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.getKeyCode(), false);
-        }
-
-        // 如果玩家走到了点则停止
-        Vec3d nextVec = Vec3d.ofCenter(nextNode.getPos());
-        if (Math.hypot(mc.thePlayer.posX - nextVec.getX(), mc.thePlayer.posZ - nextVec.getZ()) < 1.5) {
+      } else {
+        KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), false);
+        if (mc.thePlayer.onGround) {
+          // 落地了 到达位置了 删除跳跃节点
+          path.remove(1);
           path.remove(0);
         }
-      } else if (nextNode instanceof Mine) {
-        // 先挖掘对应的方块
-        BlockPos footBlock = nextNode.getPos();
-        BlockPos headBlock = nextNode.getPos().up();
+      }
+    }
+  }
 
-        Rotation footBlockRotation = RotationUtil.vec3ToRotation(Vec3d.ofCenter(footBlock));
-        Rotation headBlockRotation = RotationUtil.vec3ToRotation(Vec3d.ofCenter(headBlock));
+  private void walkExec(Node nextNode, Minecraft mc, Node node) {
+    // 转换到角度
+    Rotation rotation = RotationUtil.vec3ToRotation(Vec3d.ofCenter(nextNode.pos));
+
+    // 移动玩家视角
+    mc.thePlayer.rotationPitch = smoothRotation(mc.thePlayer.rotationPitch, rotation.getPitch(), new Random().nextFloat() / 5);
+    mc.thePlayer.rotationYaw = smoothRotation(mc.thePlayer.rotationYaw, rotation.getYaw(), 75F);
+
+    // 控制行走
+    KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), true);
+    KeyBinding.setKeyBindState(mc.gameSettings.keyBindSneak.getKeyCode(), false);
+
+    // 利用node中记录的rotation与实际rotation的差值 通过a和d修正玩家位置
+    if (Math.abs(mc.thePlayer.rotationYaw - rotation.getYaw()) < 0.5) {
+      // 开始纠正位置
+      float yawShould = node.nextRotation.getYaw();
+      float yawNow = rotation.getYaw();
+
+      // 经过研究 发现如果正负号不统一的 并且角度小于-90度的 则需要将负号改为正号
+      if (yawNow < -90 && yawShould > 90) {
+        yawNow = -1 * yawNow;
+      } else if (yawNow > 90 && yawShould < -90) {
+        yawShould = -1 * yawShould;
+      }
+
+      // 如果 now - should是正的 则偏左 则需要往右移动
+      if ((yawNow - yawShould) > 2) {
+        MelodySkyPlus.LOGGER.info("Found Player offset to the left, start to go to the right.");
+        KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.getKeyCode(), true);
+      } else {
+        KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.getKeyCode(), false);
+      }
+
+      // 反之亦然
+      if ((yawNow - yawShould) < -2) {
+        MelodySkyPlus.LOGGER.info("Found Player offset to the right, start to go to the left.");
+        KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.getKeyCode(), true);
+      } else {
+        KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.getKeyCode(), false);
+      }
+    } else {
+      KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.getKeyCode(), false);
+      KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.getKeyCode(), false);
+    }
 
 
-        if (mc.theWorld.getBlockState(headBlock).getBlock() != Blocks.air) {
-          // 停止走路 拒接转圈
-          KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), false);
-          KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.getKeyCode(), false);
-          KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.getKeyCode(), false);
+  }
 
-          // 转头到方块
-          mc.thePlayer.rotationPitch = smoothRotation(mc.thePlayer.rotationPitch, headBlockRotation.getPitch(), 25);
-          mc.thePlayer.rotationYaw = smoothRotation(mc.thePlayer.rotationYaw, headBlockRotation.getYaw(), 25);
+  private void mineExec(Node nextNode, Minecraft mc, List<Node> path) {
+    // 先挖掘对应的方块
+    BlockPos footBlock = nextNode.getPos();
+    BlockPos headBlock = nextNode.getPos().up();
 
-          // 挖掘路障
-          KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), RotationUtil.isLookingAtBlock(headBlock));
-        } else if (mc.theWorld.getBlockState(footBlock).getBlock() != Blocks.air) {
-          // 停止走路 拒接转圈
-          KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), false);
-          KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.getKeyCode(), false);
-          KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.getKeyCode(), false);
+    Rotation footBlockRotation = RotationUtil.vec3ToRotation(Vec3d.ofCenter(footBlock));
+    Rotation headBlockRotation = RotationUtil.vec3ToRotation(Vec3d.ofCenter(headBlock));
 
-          // 转头到方块
-          mc.thePlayer.rotationPitch = smoothRotation(mc.thePlayer.rotationPitch, footBlockRotation.getPitch(), 25);
-          mc.thePlayer.rotationYaw = smoothRotation(mc.thePlayer.rotationYaw, footBlockRotation.getYaw(), 25);
 
-          KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), RotationUtil.isLookingAtBlock(footBlock));
-        } else {
-          // 停止挖掘
-          KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), false);
+    if (mc.theWorld.getBlockState(headBlock).getBlock() != Blocks.air) {
+      // 停止走路 拒接转圈
+      KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), false);
+      KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.getKeyCode(), false);
+      KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.getKeyCode(), false);
 
-          // 通路后 先转头
-          mc.thePlayer.rotationPitch = smoothRotation(mc.thePlayer.rotationPitch, footBlockRotation.getPitch(), new Random().nextFloat() / 5);
-          mc.thePlayer.rotationYaw = smoothRotation(mc.thePlayer.rotationYaw, footBlockRotation.getYaw(), 25F);
+      // 转头到方块
+      mc.thePlayer.rotationPitch = smoothRotation(mc.thePlayer.rotationPitch, headBlockRotation.getPitch(), 25);
+      mc.thePlayer.rotationYaw = smoothRotation(mc.thePlayer.rotationYaw, headBlockRotation.getYaw(), 25);
 
-          // 转弯头开始走
-          KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), Math.abs(mc.thePlayer.rotationYaw - footBlockRotation.getYaw()) < 5);
+      // 挖掘路障
+      KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), RotationUtil.isLookingAtBlock(headBlock));
+    } else if (mc.theWorld.getBlockState(footBlock).getBlock() != Blocks.air) {
+      // 停止走路 拒接转圈
+      KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), false);
+      KeyBinding.setKeyBindState(mc.gameSettings.keyBindLeft.getKeyCode(), false);
+      KeyBinding.setKeyBindState(mc.gameSettings.keyBindRight.getKeyCode(), false);
 
-          // 处理下一个点
-          int x = (int) (mc.thePlayer.posX - mc.thePlayer.posX % 1 - 1);
-          int y = (int) (mc.thePlayer.posY - mc.thePlayer.posY % 1);
-          int z = (int) (mc.thePlayer.posZ - mc.thePlayer.posZ % 1 - 1);
-          BlockPos posPlayer = new BlockPos(x, y, z); // Minecraft提供的.getPosition不好用 返回的位置经常有较大的误差 这样是最保险的
-          if (nextNode.getPos().equals(posPlayer)) {
-            path.remove(0);
-            KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), false);
-          }
-        }
+      // 转头到方块
+      mc.thePlayer.rotationPitch = smoothRotation(mc.thePlayer.rotationPitch, footBlockRotation.getPitch(), 25);
+      mc.thePlayer.rotationYaw = smoothRotation(mc.thePlayer.rotationYaw, footBlockRotation.getYaw(), 25);
+
+      KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), RotationUtil.isLookingAtBlock(footBlock));
+    } else {
+      // 停止挖掘
+      KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), false);
+
+      // 通路后 先转头
+      mc.thePlayer.rotationPitch = smoothRotation(mc.thePlayer.rotationPitch, footBlockRotation.getPitch(), new Random().nextFloat() / 5);
+      mc.thePlayer.rotationYaw = smoothRotation(mc.thePlayer.rotationYaw, footBlockRotation.getYaw(), 25F);
+
+      // 转弯头开始走
+      KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), Math.abs(mc.thePlayer.rotationYaw - footBlockRotation.getYaw()) < 5);
+
+      // 处理下一个点
+      int x = (int) (mc.thePlayer.posX - mc.thePlayer.posX % 1 - 1);
+      int y = (int) (mc.thePlayer.posY - mc.thePlayer.posY % 1);
+      int z = (int) (mc.thePlayer.posZ - mc.thePlayer.posZ % 1 - 1);
+      BlockPos posPlayer = new BlockPos(x, y, z); // Minecraft提供的.getPosition不好用 返回的位置经常有较大的误差 这样是最保险的
+      if (nextNode.getPos().equals(posPlayer)) {
+        path.remove(0);
+        KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), false);
       }
     }
   }
