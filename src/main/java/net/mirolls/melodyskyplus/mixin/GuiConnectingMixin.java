7473d586ceaddac5ad6f98d2871257a21b3ecb0e
liangmiQwQ <mi_yyds@126.com>
2025-04-19 19:54:14 +0800

    refactor: refactor the struct of Project

--- src/main/java/net/mirolls/melodyskyplus/mixin/GuiConnectingMixin.java
+++ src/main/java/net/mirolls/melodyskyplus/mixin/GuiConnectingMixin.java
@@ -1,6 +1,7 @@
 package net.mirolls.melodyskyplus.mixin;
 
 import net.minecraft.client.multiplayer.GuiConnecting;
+import net.mirolls.melodyskyplus.client.AntiBug;
 import net.mirolls.melodyskyplus.modules.AutoReconnect.AutoReconnect;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,6 +14,8 @@ import java.util.Objects;
 public class GuiConnectingMixin {
   @Inject(method = "connect", at = @At("HEAD"))
   private void connect(String host, int port, CallbackInfo ci) {
-    Objects.requireNonNull(AutoReconnect.getInstance()).host = host + ":" + port;
+    if (AntiBug.isBugRemoved()) {
+      Objects.requireNonNull(AutoReconnect.getInstance()).host = host + ":" + port;
+    }
   }
 }
--- src/main/java/net/mirolls/melodyskyplus/mixin/GuiDisconnectedMixin.java
+++ src/main/java/net/mirolls/melodyskyplus/mixin/GuiDisconnectedMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.multiplayer.ServerData;
 import net.minecraftforge.fml.client.FMLClientHandler;
+import net.mirolls.melodyskyplus.client.AntiBug;
 import net.mirolls.melodyskyplus.modules.AutoReconnect.AutoReconnect;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -24,68 +25,76 @@ public class GuiDisconnectedMixin extends GuiScreen {
 
   @Inject(method = "initGui", at = @At("RETURN"))
   public void initGui(CallbackInfo ci) {
-    buttonList.add(new GuiButton(8090,
-            this.width / 2 - 100,
-            this.height / 2 + this.field_175353_i / 2 + this.fontRendererObj.FONT_HEIGHT + GAP,
-            "Reconnect"
-        )
-    );
-    buttonList.add(new GuiButton(8091,
-            this.width / 2 - 100,
-            this.height / 2 + this.field_175353_i / 2 + this.fontRendererObj.FONT_HEIGHT + GAP * 2,
-            "Auto Reconnect"
-        )
-    );
-    if (Objects.requireNonNull(AutoReconnect.getInstance()).isEnabled()) {
-      reconnecting = true;
-      // 自动重新链接
-      autoReconnect();
+    if (AntiBug.isBugRemoved()) {
+      buttonList.add(new GuiButton(8090,
+              this.width / 2 - 100,
+              this.height / 2 + this.field_175353_i / 2 + this.fontRendererObj.FONT_HEIGHT + GAP,
+              "Reconnect"
+          )
+      );
+      buttonList.add(new GuiButton(8091,
+              this.width / 2 - 100,
+              this.height / 2 + this.field_175353_i / 2 + this.fontRendererObj.FONT_HEIGHT + GAP * 2,
+              "Auto Reconnect"
+          )
+      );
+      if (Objects.requireNonNull(AutoReconnect.getInstance()).isEnabled()) {
+        reconnecting = true;
+        // 自动重新链接
+        autoReconnect();
+      }
     }
   }
 
   @Inject(method = "actionPerformed", at = @At("RETURN"))
   protected void actionPerformed(GuiButton button, CallbackInfo ci) {
-    if (button.id == 8090) {
-      reconnecting = true;
-      reconnect();
-    }
-    if (button.id == 8091) {
-      boolean setValue = !Objects.requireNonNull(AutoReconnect.getInstance()).isEnabled();
-      Objects.requireNonNull(AutoReconnect.getInstance()).setEnabled(setValue);
-      if (setValue && !reconnecting) {
+    if (AntiBug.isBugRemoved()) {
+      if (button.id == 8090) {
         reconnecting = true;
-        autoReconnect();
-      } else if (!setValue && reconnecting) {
-        reconnecting = false;
-        // 进行设置
+        reconnect();
+      }
+      if (button.id == 8091) {
+        boolean setValue = !Objects.requireNonNull(AutoReconnect.getInstance()).isEnabled();
+        Objects.requireNonNull(AutoReconnect.getInstance()).setEnabled(setValue);
+        if (setValue && !reconnecting) {
+          reconnecting = true;
+          autoReconnect();
+        } else if (!setValue && reconnecting) {
+          reconnecting = false;
+          // 进行设置
+        }
       }
     }
   }
 
   private void reconnect() {
-    if (reconnecting) {
-      reconnecting = false;
-      mc.addScheduledTask(() -> {
-        FMLClientHandler.instance().connectToServer(new GuiMainMenu(), new ServerData("server", Objects.requireNonNull(AutoReconnect.getInstance()).host, false));
-      });
+    if (AntiBug.isBugRemoved()) {
+      if (reconnecting) {
+        reconnecting = false;
+        mc.addScheduledTask(() -> {
+          FMLClientHandler.instance().connectToServer(new GuiMainMenu(), new ServerData("server", Objects.requireNonNull(AutoReconnect.getInstance()).host, false));
+        });
+      }
     }
   }
 
   private void autoReconnect() {
-    Objects.requireNonNull(AutoReconnect.getInstance()).reconnect((second) -> {
-      GuiButton reconnectButton = null;
-      for (GuiButton button : buttonList) {
-        if (button.id == 8091) {
-          reconnectButton = button;
+    if (AntiBug.isBugRemoved()) {
+      Objects.requireNonNull(AutoReconnect.getInstance()).reconnect((second) -> {
+        GuiButton reconnectButton = null;
+        for (GuiButton button : buttonList) {
+          if (button.id == 8091) {
+            reconnectButton = button;
+          }
         }
-      }
-      if (reconnectButton != null) {
-        reconnectButton.displayString = "Auto Reconnect" + " (" + second + ")";
-      }
-    }, () -> {
-      if (AutoReconnect.getInstance().isEnabled()) {
-        reconnect();
-      }
-    });
+        if (reconnectButton != null) {
+          reconnectButton.displayString = "Auto Reconnect" + " (" + second + ")";
+        }
+      }, () -> {
+        if (AutoReconnect.getInstance().isEnabled()) {
+          reconnect();
+        }
+      });
+    }
   }
 }
