7ee98df9cbb7dca7ee42e16fefef94aed3149bde
liangmiQwQ <mi_yyds@126.com>
2025-05-18 08:32:17 +0800

    feat: add strongly EtherWarpUtils

--- src/main/java/net/mirolls/melodyskyplus/utils/EtherWarpUtils.java
+++ src/main/java/net/mirolls/melodyskyplus/utils/EtherWarpUtils.java
@@ -1,43 +1,81 @@
 package net.mirolls.melodyskyplus.utils;
 
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.Minecraft;
 import net.minecraft.util.BlockPos;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class EtherWarpUtils {
-  private Map<BlockPos, IBlockState> blockStateMap = new HashMap<>();
 
-  public List<BlockPos> findWayToEtherWarp(BlockPos end) {
-    return findWayToEtherWarp(1, end);
+  public static List<BlockPos> findWayToEtherWarp(BlockPos end, int maxLayer, int radius) {
+    return findWayToEtherWarp(1, end, maxLayer, radius);
   }
 
-  public List<BlockPos> findWayToEtherWarp(int layer, BlockPos end) {
-    List<BlockPos> routes = findRoutesWithLayer(layer, end);
-    if (routes.isEmpty()) {
-      return findWayToEtherWarp(layer + 1, end);
+  public static List<BlockPos> findWayToEtherWarp(int layer, BlockPos end, int maxLayer, int radius) {
+    List<BlockPos> routes = findRoutesWithLayer(layer, end, radius);
+    if (routes.isEmpty() && layer <= maxLayer) {
+      return findWayToEtherWarp(layer + 1, end, maxLayer, radius);
     }
 
     return routes;
   }
 
-  private List<BlockPos> findRoutesWithLayer(int layer, BlockPos end) {
-    if (layer == 1) {
-      if (PlayerUtils.rayTrace(end)) {
-        return new ArrayList<>(Collections.singletonList(end));
+  private static List<BlockPos> findRoutesWithLayer(int layer, BlockPos end, int radius) {
+    Map<Integer, Set<EtherWarpPos>> layerMap = new HashMap<>();
+
+    // 先加入第0层
+    layerMap.put(0, new HashSet<>());
+    layerMap.get(0).add(new EtherWarpPos(end, null));
+
+    for (int i = 1; i < layer; i++) { // 这里特意少做一层 最后直接用PlayerUtils.rayTrace获取
+      // 每一层单独处理
+      Set<EtherWarpPos> thisLayer = new HashSet<>();
+
+      for (EtherWarpPos lastPos : layerMap.get(i - 1)) {
+        // 面对上一层的所有点展开搜查
+        for (BlockPos pos : BlockPos.getAllInBox(end.add(-radius, -radius, -radius), end.add(radius, radius, radius))) {
+          if (PlayerUtils.rayTrace(pos, lastPos.pos)) {
+            // 存入当前层
+            thisLayer.add(new EtherWarpPos(pos, lastPos));
+          }
+        }
+      }
+
+      layerMap.put(i, thisLayer);
+    }
+
+    // 获取最后一层
+    for (EtherWarpPos etherWarpPos : layerMap.get(layer - 1)) {
+      if (PlayerUtils.rayTrace(etherWarpPos.pos)) {
+        // 找到path了 进行构建
+        List<EtherWarpPos> path = new ArrayList<>();
+        path.add(etherWarpPos);
+
+        List<EtherWarpPos> builtPath = buildPath(path);
+
+        return builtPath.stream().map((e) -> e.pos).collect(Collectors.toList());
       }
     }
+
+    return new ArrayList<>();
   }
 
-
-  private IBlockState getBlockState(BlockPos pos) {
-    if (blockStateMap.containsKey(pos)) {
-      return blockStateMap.get(pos);
+  private static List<EtherWarpPos> buildPath(List<EtherWarpPos> currentPath) {
+    if (currentPath.get(currentPath.size() - 1).parent != null) {
+      currentPath.add(currentPath.get(currentPath.size() - 1).parent);
+      return buildPath(currentPath);
     } else {
-      IBlockState blockState = Minecraft.getMinecraft().theWorld.getBlockState(pos);
-      blockStateMap.put(pos, blockState);
-      return blockState;
+      return currentPath;
     }
+  }
+}
+
+class EtherWarpPos {
+  BlockPos pos;
+  EtherWarpPos parent;
+
+  public EtherWarpPos(BlockPos pos, EtherWarpPos parent) {
+    this.pos = pos;
+    this.parent = parent;
   }
 }
