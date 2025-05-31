0ffb2c78ab15560693b8cfae216c9caf6657c37c
liangmiQwQ <mi_yyds@126.com>
2025-06-01 07:15:56 +0800

    feat: add basic MiningSkillMixin

---
+++ src/main/java/net/mirolls/melodyskyplus/mixin/MiningSkillMixin.java
@@ -0,0 +1,8 @@
+package net.mirolls.melodyskyplus.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import xyz.Melody.module.modules.macros.Mining.MiningSkill;
+
+@Mixin(MiningSkill.class)
+public class MiningSkillMixin {
+}
