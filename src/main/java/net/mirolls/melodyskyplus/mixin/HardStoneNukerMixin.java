4036017ae6a8d54235221cb46ecb33678d7e58a8
liangmiQwQ <mi_yyds@126.com>
2025-06-11 20:59:38 +0800

    feat: add something basicly in 1.3.2

---
+++ src/main/java/net/mirolls/melodyskyplus/mixin/HardStoneNukerMixin.java
@@ -0,0 +1,8 @@
+package net.mirolls.melodyskyplus.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import xyz.Melody.module.modules.macros.Mining.HardStoneNuker;
+
+@Mixin(HardStoneNuker.class)
+public class HardStoneNukerMixin {
+}
