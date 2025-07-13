3b6128a461df16bc75f03255f76eff9d76de1d6f
liangmiQwQ <mi_yyds@126.com>
2025-07-13 16:20:16 +0800

    feat: add auto mode

--- src/main/java/net/mirolls/melodyskyplus/libs/PickaxeAbility.java
+++ src/main/java/net/mirolls/melodyskyplus/libs/PickaxeAbility.java
@@ -4,21 +4,53 @@ import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.lang.reflect.Field;
+import java.util.regex.Pattern;
+
 public class PickaxeAbility {
-  private boolean pickaxeAbility = false;
+  public boolean check = false;
+  private boolean pickaxeAbility;
 
   public PickaxeAbility() {
     MinecraftForge.EVENT_BUS.register(this);
   }
 
   @SubscribeEvent
-  public void onPickaxeAbility(ClientChatReceivedEvent event) {
+  public void onPickaxeAbility(ClientChatReceivedEvent event) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
     String message = event.message.getUnformattedText();
 
     if (message.contains("You used your Mining Speed Boost Pickaxe Ability!")) {
       this.pickaxeAbility = true;
     } else if (message.contains("Your Mining Speed Boost has expired!")) {
       this.pickaxeAbility = false;
+    }
+
+    if (check) {
+      if (Pattern.compile("^(.*?) is now available!$").matcher(message).matches()) {
+        // ready
+        Class<?> client = Class.forName("xyz.Melody.Client");
+        Field pickaxeField = client.getDeclaredField("pickaxeAbilityReady");
+        pickaxeField.setAccessible(true);
+        pickaxeField.set(client, true);
+      } else if (Pattern.compile("^You used your (.*?) Pickaxe Ability!$").matcher(message).matches()) {
+        // used
+        Class<?> client = Class.forName("xyz.Melody.Client");
+        Field pickaxeField = client.getDeclaredField("pickaxeAbilityReady");
+        Field pickaxeField2 = client.getDeclaredField("pickaxeAbility");
+        pickaxeField.setAccessible(true);
+        pickaxeField2.setAccessible(true);
+        pickaxeField.set(client, false);
+        pickaxeField2.set(client, true);
+      } else if (Pattern.compile("^Your (.*?) has expired!$").matcher(message).matches()) {
+        // expire
+        Class<?> client = Class.forName("xyz.Melody.Client");
+        Field pickaxeField = client.getDeclaredField("pickaxeAbilityReady");
+        Field pickaxeField2 = client.getDeclaredField("pickaxeAbility");
+        pickaxeField.setAccessible(true);
+        pickaxeField2.setAccessible(true);
+        pickaxeField.set(client, false);
+        pickaxeField2.set(client, false);
+      }
     }
   }
 
--- src/main/java/net/mirolls/melodyskyplus/mixin/MiningSkillMixin.java
+++ src/main/java/net/mirolls/melodyskyplus/mixin/MiningSkillMixin.java
@@ -3,11 +3,13 @@ package net.mirolls.melodyskyplus.mixin;
 import net.mirolls.melodyskyplus.MelodySkyPlus;
 import net.mirolls.melodyskyplus.client.AntiBug;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.Melody.Event.value.Option;
+import xyz.Melody.Event.value.TextValue;
 import xyz.Melody.Event.value.Value;
 import xyz.Melody.module.modules.macros.Mining.MiningSkill;
 
@@ -16,7 +18,15 @@ import java.util.Arrays;
 
 @Mixin(value = MiningSkill.class, remap = false)
 public class MiningSkillMixin {
+  @Shadow
+  public TextValue<String> ready;
+  @Shadow
+  public TextValue<String> used;
+  @Shadow
+  public TextValue<String> expire;
   public Option<Boolean> melodySkyPlus$useRod = new Option<>("Use Rod", false);
+  public Option<Boolean> melodySkyPlus$autoMode;
+
 
   @SuppressWarnings("rawtypes")
   @ModifyArg(method = "<init>",
@@ -24,7 +34,22 @@ public class MiningSkillMixin {
       index = 0)
   public Value[] addValueArgs(Value[] originalValues) {
     if (AntiBug.isBugRemoved()) {
-      Value[] returnValues = Arrays.copyOf(originalValues, originalValues.length + 1);
+
+      melodySkyPlus$autoMode = new Option<>("Auto Mode", true, (val) -> {
+        if (MiningSkill.getINSTANCE() != null) {
+          MelodySkyPlus.pickaxeAbility.check = true;
+          
+          ready.setValue(val ? "🪷𬺈〾🝼⇌🝼⻯" : "Mining Speed Boost is now available!");
+          ready.setEnabled(val);
+          used.setValue(val ? "🪷𬺈〾🝼⇌🝼⻯" : "You used your Mining Speed Boost Pickaxe Ability!");
+          used.setEnabled(val);
+          expire.setValue(val ? "🪷𬺈〾🝼⇌🝼⻯" : "Your Mining Speed Boost has expired!");
+          expire.setEnabled(val);
+        }
+      });
+
+      Value[] returnValues = Arrays.copyOf(originalValues, originalValues.length + 2);
+      returnValues[returnValues.length - 2] = melodySkyPlus$autoMode;
       returnValues[returnValues.length - 1] = melodySkyPlus$useRod;
 
       return returnValues;
