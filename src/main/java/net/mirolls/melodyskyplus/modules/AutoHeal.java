3e4164699b59cd3ecf2eb0e62a9774bc9a282451
liangmiQwQ <mi_yyds@126.com>
2025-08-19 21:16:19 +0800

    feat: add log and limit

--- src/main/java/net/mirolls/melodyskyplus/modules/AutoHeal.java
+++ src/main/java/net/mirolls/melodyskyplus/modules/AutoHeal.java
@@ -6,6 +6,8 @@ import xyz.Melody.Event.EventHandler;
 import xyz.Melody.Event.events.world.EventTick;
 import xyz.Melody.Event.value.Numbers;
 import xyz.Melody.System.Managers.Client.ModuleManager;
+import xyz.Melody.Utils.Helper;
+import xyz.Melody.Utils.timer.TimerUtil;
 import xyz.Melody.module.Module;
 import xyz.Melody.module.ModuleType;
 
@@ -16,10 +18,12 @@ public class AutoHeal extends ModulePlus {
   private final Numbers<Double> limit = new Numbers<>("Limit(%)", 30.0, 1.0, 99.0, 1.0);
   private final Numbers<Double> slot = new Numbers<>("Heal Item Slot", 8.0, 1.0, 8.0, 1.0);
   HealStatus healStatus = HealStatus.getCleanStatus();
+  TimerUtil cooldownTimer = new TimerUtil();
   List<Module> mods = new ArrayList<>();
 
   public AutoHeal() {
     super("AutoHeal", ModuleType.QOL);
+    cooldownTimer.reset();
     this.setModInfo("Auto use heal item when you low");
     this.addValues(limit, slot);
     this.except();
@@ -33,12 +37,14 @@ public class AutoHeal extends ModulePlus {
       return;
     }
 
+
     float health = mc.thePlayer.getHealth();
     float maxHealth = mc.thePlayer.getMaxHealth();
     float healthPercentage = (health / maxHealth) * 100;
 
-    if (healthPercentage <= limit.getValue()) {
+    if (healthPercentage <= limit.getValue() && cooldownTimer.hasReached(10_000)) {
       // 玩家生命值低于阈值，治疗
+      Helper.sendMessage("[AutoHeal] You're not healthy, need heal.");
       performHealAction();
     }
   }
@@ -60,6 +66,7 @@ public class AutoHeal extends ModulePlus {
     if (healStatus.tick == 30) {
       reEnableMacros();
       healStatus = HealStatus.getCleanStatus();
+      cooldownTimer.reset();
     }
   }
 
@@ -90,6 +97,7 @@ public class AutoHeal extends ModulePlus {
   }
 
   public void onEnable() {
+    cooldownTimer.reset();
     super.onEnable();
   }
 
