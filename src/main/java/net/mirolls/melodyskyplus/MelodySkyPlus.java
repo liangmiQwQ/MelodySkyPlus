5f5af0b6df304d162ad64ae9cb860f156c61df7e
liangmiQwQ <mi_yyds@126.com>
2025-10-08 10:50:46 +0800

    chore: release 2.0.0

--- src/main/java/net/mirolls/melodyskyplus/MelodySkyPlus.java
+++ src/main/java/net/mirolls/melodyskyplus/MelodySkyPlus.java
@@ -12,13 +12,10 @@ import net.mirolls.melodyskyplus.path.exec.PathExec;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
-
 @Mod(modid = MelodySkyPlus.MODID, version = MelodySkyPlus.VERSION)
 public class MelodySkyPlus {
   public static final String MODID = "melodyskyplus";
-  public static final String VERSION = "1.3.9";
-  public static final String MELODY_VERSION = "2.15.0";
+  public static final String VERSION = "2.0.0";
   public static final Logger LOGGER = LogManager.getLogger(MelodySkyPlus.MODID);
   public static RotationLib rotationLib;
   public static CheckPlayerFlying checkPlayerFlying;
@@ -61,11 +58,5 @@ public class MelodySkyPlus {
     miningSkillExecutor = new MiningSkillExecutor();
     drinkingLib = new DrinkingLib();
     // events
-
-    try {
-      Verify.verify();
-    } catch (IOException e) {
-      LOGGER.error("Cannot Verify Your Account!" + e);
-    }
   }
 }
